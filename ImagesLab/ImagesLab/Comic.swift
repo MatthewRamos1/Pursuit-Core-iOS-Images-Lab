10c1bcc9defcc45e806e4c83f306e28d173dd17e
Matthew Ramos <mmramos309@gmail.com>
2019-12-13 16:46:22 -0500

    Created view controller, aligned stepper, text bar, and buttons. Set up struct for Comic data to be parsed.

---
+++ ImagesLab/ImagesLab/Comic.swift
@@ -0,0 +1,14 @@
+//
+//  Comic.swift
+//  ImagesLab
+//
+//  Created by Matthew Ramos on 12/13/19.
+//  Copyright © 2019 Matthew Ramos. All rights reserved.
+//
+
+import Foundation
+
+struct Comic {
+    let num: Int
+    let img: String
+}
---
+++ ImagesLab/ImagesLab/ComicViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ComicViewController.swift
+//  ImagesLab
+//
+//  Created by Matthew Ramos on 12/12/19.
+//  Copyright © 2019 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ComicViewController: UIViewController {
+    @IBOutlet weak var comicViewController: UIImageView!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func mostRecentButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func randomButtonPressed(_ sender: UIButton) {
+    }
+    
+}
+
